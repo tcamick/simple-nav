39ec45e241803dd6afa51a727f72d1d4c218602e
tcamick <amickcole@gmail.com>
2015-09-28 01:51:31 -0500

    fixing tabbing

--- simplenav.js
+++ simplenav.js
@@ -1,16 +1,16 @@
 $(function() {
-
-// Dropdown toggle
-$('.dropdown-toggle').click(function(){
-$(this).next('.dropdown').toggle();
-});
-
-$(document).click(function(e) {
-var target = e.target;
-if (!$(target).is('.dropdown-toggle') && !$(target).parents().is('.dropdown-toggle')) {
-  $('.dropdown').hide();
-}
-});
-
+    
+    // Dropdown toggle
+    $('.dropdown-toggle').click(function(){
+	$(this).next('.dropdown').toggle();
+    });
+    
+    $(document).click(function(e) {
+	var target = e.target;
+	if (!$(target).is('.dropdown-toggle') && !$(target).parents().is('.dropdown-toggle')) {
+	    $('.dropdown').hide();
+	}
+    });
+    
 });
 
