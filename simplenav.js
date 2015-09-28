9792c198a5912046ec00604c51ed56d38f2aedaa
tcamick <amickcole@gmail.com>
2015-09-28 01:33:11 -0500

    first commit

---
+++ simplenav.js
@@ -0,0 +1,16 @@
+$(function() {
+
+// Dropdown toggle
+$('.dropdown-toggle').click(function(){
+$(this).next('.dropdown').toggle();
+});
+
+$(document).click(function(e) {
+var target = e.target;
+if (!$(target).is('.dropdown-toggle') && !$(target).parents().is('.dropdown-toggle')) {
+  $('.dropdown').hide();
+}
+});
+
+});
+
